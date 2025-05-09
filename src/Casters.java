118307b95ad380e7e37a66fc47aafbd316974e2c
LucasLonegro <llonegrogurfinkel@itba.edu.ar>
2025-05-09 09:23:00 +0200

    add sample solution

---
+++ src/Casters.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class Casters {
+
+    public static List<String> noopCaster(String line){
+        return List.of(line);
+    }
+}
--- src/RDFCaster.java
+++ src/RDFCaster.java
@@ -1,17 +1,28 @@
-import java.io.Reader;
+import java.io.*;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
 
 public class RDFCaster {
 
-    public static void cast(Reader in, AsyncStringWriter out, Function<String, List<String>> rowCaster) {
+    public static void cast(InputStream in, AsyncStringWriter out, Function<String, List<String>> rowCaster) {
         Scanner scanner = new Scanner(in).useDelimiter("\n");
         scanner.next(); // ignore headers
 
         scanner.forEachRemaining((line) -> {
             out.write(rowCaster.apply(line));
         });
+    }
+
+    public static void main(String[] args) {
+        try (InputStream in = new FileInputStream("Resources/Assigned_Hours.csv");
+             Writer out = new FileWriter("out.txt")) {
+            AsyncStringWriter asyncWriter = new AsyncStringWriter(out);
+            cast(in, asyncWriter, Casters::noopCaster);
+            asyncWriter.shutdown();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
